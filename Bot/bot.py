db90083862fda2450e46829cdfdcd7a6cacb333b
HKMUSIC <himanshu4hk9@gmail.com>
2025-11-11 11:12:32 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -23,7 +23,7 @@ from mustjoin import check_join
 from config import BOT_TOKEN, ADMIN_IDS
 
 # ================= MongoDB Setup =================
-MONGO_URI = os.getenv("MONGO_URI") or "mongodb+srv://Hkbots:hk123@gamechanger0.ck6qqyl.mongodb.net/?retryWrites=true&w=majority&appName=Gamechanger0"
+MONGO_URI = os.getenv("MONGO_URI") or "mongodb+srv://Gojowaifu:Gojowaifu123@gojowaifu.55aic9l.mongodb.net/?retryWrites=true&w=majority&appName=Gojowaifu"
 client = MongoClient(MONGO_URI)
 db = client["QuickCodes"]
 users_col = db["users"]
